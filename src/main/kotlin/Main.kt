37c5156b09e2d6f5010e42d42703cfc7141333d0
Tr3ble <tr3ble@outlook.com>
2023-06-27 03:03:29 +0300

    Refactor Wikipedia search code for better readability and modularity - Extract user prompts for language and query into separate functions - Create separate functions to build search and article URLs - Separate result extraction and display logic into individual functions Improved XML handling code - Optimized getXmlDocument function:     Explicit imports were defined for better readability.     Set request method for HttpURLConnection.     Resource handling was enhanced using runCatching and use for automatic closure.     Connection closing now takes place in the onFailure block. - Optimized removeXmlTags function:     Replaced StringBuilder usage with Kotlin's buildString extension for simplifying string building
    Powered by AI Assistent by JetBrains, lol

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,84 @@
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
+import org.w3c.dom.Document
 import javax.xml.parsers.DocumentBuilderFactory
+import java.io.InputStream
 
 fun main() {
-    print("Choose a language to search for articles (en, ru, fr and others): ")
-    val language = readLine() ?: "en"
+    val language = promptLanguage()
+    val query = promptQuery()
 
-            print("Enter your request for wikipedia: ")
-    val query = readLine() ?: ""
-
-    val searchUrl = "https://$language.wikipedia.org/w/api.php?action=query&list=search&srsearch=${URLEncoder.encode(query, "UTF-8")}&format=xml"
+    val searchUrl = buildSearchUrl(language, query)
     val searchDoc = getXmlDocument(searchUrl)
+    val pElement = extractFirstSearchResult(searchDoc)
 
-    val pElement = searchDoc.getElementsByTagName("p").item(0) as org.w3c.dom.Element
     val title = pElement.getAttribute("title")
     val snippet = pElement.getAttribute("snippet")
     val pageid = pElement.getAttribute("pageid")
 
-    val articleUrl = "https://$language.wikipedia.org/w/api.php?action=query&prop=extracts&pageids=$pageid&format=xml"
+    val articleUrl = buildArticleUrl(language, pageid)
     val articleDoc = getXmlDocument(articleUrl)
-    val extractElement = articleDoc.getElementsByTagName("extract").item(0)
-    val articleText = extractElement.textContent
+    val articleText = extractArticleText(articleDoc)
 
     val snippetWithoutTags = removeXmlTags(snippet)
     val articleTextWithoutTags = removeXmlTags(articleText)
 
+    displayResult(language, title, snippetWithoutTags, articleTextWithoutTags)
+}
+
+fun promptLanguage(): String {
+    print("Choose a language to search for articles (en, ru, fr and others): ")
+    return readLine() ?: "en"
+}
+
+fun promptQuery(): String {
+    print("Enter your request for wikipedia: ")
+    return readLine() ?: ""
+}
+
+fun buildSearchUrl(language: String, query: String): String =
+    "https://$language.wikipedia.org/w/api.php?action=query&list=search&srsearch=${URLEncoder.encode(query, "UTF-8")}&format=xml"
+
+fun buildArticleUrl(language: String, pageid: String): String =
+    "https://$language.wikipedia.org/w/api.php?action=query&prop=extracts&pageids=$pageid&format=xml"
+
+fun extractFirstSearchResult(searchDoc: org.w3c.dom.Document): org.w3c.dom.Element =
+    searchDoc.getElementsByTagName("p").item(0) as org.w3c.dom.Element
+
+fun extractArticleText(articleDoc: org.w3c.dom.Document): String =
+    articleDoc.getElementsByTagName("extract").item(0).textContent
+
+fun displayResult(language: String, title: String, snippet: String, articleText: String) {
     println("URL: https://$language.wikipedia.org/wiki/$title")
     println("Heading: $title")
-    println("Snippet: $snippetWithoutTags")
-    println("Text of the article: $articleTextWithoutTags")
+    println("Snippet: $snippet")
+    println("Text of the article: $articleText")
 }
 
-fun getXmlDocument(url: String): org.w3c.dom.Document {
-    val connection = URL(url).openConnection() as HttpURLConnection
-    val inputStream = connection.inputStream
-    val factory = DocumentBuilderFactory.newInstance()
-    val builder = factory.newDocumentBuilder()
-    val doc = builder.parse(inputStream)
-    connection.disconnect()
-    inputStream.close()
-    return doc
+fun getXmlDocument(url: String): Document {
+    val connection = (URL(url).openConnection() as HttpURLConnection).apply {
+        requestMethod = "GET"
+    }
+
+    return connection.runCatching {
+        inputStream.use { inputStream ->
+            val factory = DocumentBuilderFactory.newInstance()
+            val builder = factory.newDocumentBuilder()
+            builder.parse(inputStream)
+        }
+    }.onFailure {
+        connection.disconnect()
+    }.getOrThrow()
 }
 
-fun removeXmlTags(str: String): String {
-    val result = StringBuilder()
+fun removeXmlTags(str: String): String = buildString {
     var insideTag = false
     for (char in str) {
         when (char) {
             '<' -> insideTag = true
             '>' -> insideTag = false
-            else -> if (!insideTag) result.append(char)
+            else -> if (!insideTag) append(char)
         }
     }
-    return result.toString()
 }
