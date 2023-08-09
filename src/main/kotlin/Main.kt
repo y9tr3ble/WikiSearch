93362212f5eb892b1f20b64209f7af57553114b1
y9tr3ble <tr3ble@outlook.com>
2023-08-09 11:00:24 +0200

    minor updates
    Added check for correct spelling of language (supported languages: Russian, English, Belarusian, Ukrainian, Italian, Portuguese, German, French)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,8 +27,16 @@ fun main() {
 }
 
 fun promptLanguage(): String {
-    print("Choose a language to search for articles (en, ru, fr and others): ")
-    return readlnOrNull() ?: "en"
+    val validLanguages = listOf("en", "ru", "fr", "de", "uk", "be", "pt", "it") // добавляем поддерживаемые языки сюда
+    var language = ""
+    while (language !in validLanguages) {
+        print("Choose a language to search for articles (en, ru, fr and others): ")
+        language = readlnOrNull() ?: ""
+        if (language !in validLanguages) {
+            println("Invalid language. Please choose from the provided options.")
+        }
+    }
+    return language
 }
 
 fun promptQuery(): String {
